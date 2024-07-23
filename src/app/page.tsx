b1bb9490be088e608049d1a6eed2660b047e8646
Mayank <mayank0real0world@gmail.com>
2024-07-23 18:45:25 +0530

    add : dashboard page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,76 @@ import { useRouter } from "next/navigation";
 import Tournaments  from '@/components/Tournament' 
 import axios from "axios";
 
+const tournaments = [
+  {
+    "title": "Tournament Title 1",
+    "status": "Ongoing",
+    "image": "http://res.cloudinary.com/doubpsp9q/image/upload/v1721737821/mad9mwiqfi8vnp3zzwjr.webp",
+    "roomId": "Oe31b70H",
+    "password": "abc123",
+    "entryPrice": "$10",
+    "mode": "Solo",
+    "map": "Erangel",
+    "winningPrice": "$1000",
+    "eligibility": "18+",
+    "owner": "John Doe",
+    "participants": "32/64",
+    "teamSize": "4",
+    "launchDate": "2023-06-01",
+    "collection": "Esports"
+  },
+  {
+    "title": "Tournament Title 2",
+    "status": "Upcoming",
+    "image": "http://res.cloudinary.com/doubpsp9q/image/upload/v1721737883/u5tad883em4evkz17vtr.webp",
+    "roomId": "Oe31b70H",
+    "password": "abc123",
+    "entryPrice": "$10",
+    "mode": "Duo",
+    "map": "Miramar",
+    "winningPrice": "$500",
+    "eligibility": "16+",
+    "owner": "Jane Doe",
+    "participants": "0/64",
+    "teamSize": "2",
+    "launchDate": "2023-07-01",
+    "collection": "Battle Royale"
+  },
+  {
+    "title": "Tournament Title 3",
+    "status": "Ended",
+    "image": "http://res.cloudinary.com/doubpsp9q/image/upload/v1721737911/xzqect0gvp9zch4oqjmg.webp",
+    "roomId": "Oe31b70H",
+    "password": "abc123",
+    "entryPrice": "$20",
+    "mode": "Squad",
+    "map": "Sanhok",
+    "winningPrice": "$2000",
+    "eligibility": "21+",
+    "owner": "Bob Smith",
+    "participants": "64/64",
+    "teamSize": "4",
+    "launchDate": "2023-05-01",
+    "collection": "Tournaments"
+  },
+  {
+    "title": "Tournament Title 4",
+    "status": "Upcoming",
+    "image": "http://res.cloudinary.com/doubpsp9q/image/upload/v1721737950/j7petixnokqqyn8okaxm.webp",
+    "roomId": "Oe31b70H",
+    "password": "abc123",
+    "entryPrice": "$15",
+    "mode": "Squad",
+    "map": "Vikendi",
+    "winningPrice": "$1500",
+    "eligibility": "18+",
+    "owner": "Alice Johnson",
+    "participants": "0/64",
+    "teamSize": "4",
+    "launchDate": "2023-08-01",
+    "collection": "Esports"
+  }
+]
 
 
 async function getTournamets() {
@@ -18,16 +88,16 @@ async function getTournamets() {
 export default   function Component() {
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
   const router = useRouter()
-  const [tournaments, setTournament] = useState([])
-  try {
-    const response  : any= getTournamets()
-    console.log(response.data)
-    if (response.data) {
-      setTournament(response.data.tournaments)
-    }
-  } catch (error) {
+  // const [tournaments, setTournament] = useState([])
+  // try {
+  //   const response  : any= getTournamets()
+  //   console.log(response.data)
+  //   if (response.data) {
+  //     setTournament(response.data.tournaments)
+  //   }
+  // } catch (error) {
     
-  }
+  // }
   useEffect(() => {
     const token = localStorage.getItem("token")
     setIsAuthenticated(!!token);
